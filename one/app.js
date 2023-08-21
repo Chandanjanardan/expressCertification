0fb6b9e53a793b5c02038774c56038c57e27fefe
Chandan Janardan <chandanjanardan@gmail.com>
2023-08-21 14:40:39 +0530

    adding first get with status result

--- one/app.js
+++ one/app.js
@@ -1,9 +1,21 @@
+const fs = require("fs")
 const express = require ("express")
 const app = express() 
 
-
-
-
+// app.get("/",(req ,res)=>{
+//     res.status(200).json({message:"Hello server"})
+// })
+const tours =JSON.parse( fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+// console.log(tours)
+app.get("/api/v1/tours",(req,res)=>{
+    res.status(200).json({
+        status:"success",
+        result:tours.length,
+        data:{
+            tours:tours
+        }
+    })
+})
 app.listen(4000,()=>{
     console.log(`Listning on port 4000....`)
 })
