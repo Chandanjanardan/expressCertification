2847374b2401268ad4d2ac8cd87aea89beac7107
Chandan Janardan <chandanjanardan@gmail.com>
2023-08-22 09:29:49 +0530

    connecting db

--- one/app.js
+++ one/app.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 const express = require ("express")
 const app = express() 
+const mongoose = require ("mongoose")
 
 // used for post json req
 app.use(express.json())
@@ -22,9 +23,33 @@ app.get("/api/v1/tours",(req,res)=>{
 
 // post 
 app.post("/api/v1/tours",(req,res)=>{
-    console.log(req.body)
-    res.send("Done")
+    // console.log(req.body)
+    const newId=tours[tours.length-1].id+1
+    const newTour=Object.assign({id:newId},req.body)
+
+    tours.push(newTour);
+    // push to the file
+    // write file not sync
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err =>{
+res.status(201).json({
+    status:"success",
+    data:{
+        
+        tours:newTour
+    }
 })
-app.listen(4000,()=>{
-    console.log(`Listning on port 4000....`)
+    })
+    
 })
+try {
+   mongoose.connect("mongodb://127.0.0.1:27017/one")
+   .then(()=>console.log("DB Connected successfully"))
+   
+    
+    
+    app.listen(4000,()=>{
+        console.log(`Listning on port 4000....`)
+    })
+} catch (error) {
+    console.log("Connection error")
+}
