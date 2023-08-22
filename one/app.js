e9cb032f738034e4d744fd397484ab6f526e7642
Chandan Janardan <chandanjanardan@gmail.com>
2023-08-22 10:10:44 +0530

    adding data to db

--- one/app.js
+++ one/app.js
@@ -1,74 +1,71 @@
-const fs = require("fs")
-const express = require ("express")
-const app = express() 
-const mongoose = require ("mongoose")
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 
-// used for post json req
-app.use(express.json())
+// Used for parsing JSON requests
+app.use(express.json());
 
-// app.get("/",(req ,res)=>{
-//     res.status(200).json({message:"Hello server"})
-// })
-const tours =JSON.parse( fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
-// console.log(tours)
-app.get("/api/v1/tours",(req,res)=>{
-    res.status(200).json({
-        status:"success",
-        result:tours.length,
-        data:{
-            tours:tours
-        }
-    })
-})
+// Read tours data from JSON file (ensure the path is correct)
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-// post 
-app.post("/api/v1/tours",(req,res)=>{
-    // console.log(req.body)
-    const newId=tours[tours.length-1].id+1
-    const newTour=Object.assign({id:newId},req.body)
-
-    tours.push(newTour);
-    // push to the file
-    // write file not sync
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err =>{
-res.status(201).json({
-    status:"success",
-    data:{
-        
-        tours:newTour
-    }
-})
-    })
-    
-})
-
-// making modal
-const tourSchema = new mongoose.Schema(
-    {
-    name:{
-        type:String,
-        require:[true,"A tour must have a name"],
-        unique:true
+// GET all tours
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    result: tours.length,
+    data: {
+      tours: tours,
     },
-    rating:{
-        type:String,
-        require:true
-    },
-    price:{
-        type:String,
-        require:[true, "A tour must have price"]
-    }
+  });
 });
-const Tour = new mongoose.model("tour",tourSchema)
-try {
-   mongoose.connect("mongodb://127.0.0.1:27017/one")
-   .then(()=>console.log("DB Connected successfully"))
-   
-    
-    
-    app.listen(4000,()=>{
-        console.log(`Listning on port 4000....`)
-    })
-} catch (error) {
-    console.log("Connection error")
-}
+
+// Define the Tour schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    required: [true, "A tour must have a rating"],
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+// Create the Tour model
+const Tour = mongoose.model("Tour", tourSchema);
+
+// Create a new tour document and save it
+const testTour = new Tour({
+  name: "The Pune Hike",
+  rating: 4.7,
+  price: 50000,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.log("Error in model", err));
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/one", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected successfully");
+    // Start the server after the database connection is established
+    app.listen(4000, () => {
+      console.log("Listening on port 4000....");
+    });
+  })
+  .catch((error) => {
+    console.error("Connection error:", error);
+  });
